1f038a1ab7f9f09056204623ed6db086f5c33d9e
ysr_feng <1282855229@qq.com>
2017-05-18 11:02:28 +0800

    Initial commit

---
+++ app/src/main/java/com/ysr/kotlinfirst/LoginInfo.kt
@@ -0,0 +1,11 @@
+package com.ysr.kotlinfirst
+
+/**
+ * Created by ysr on 2017/5/18 上午10:52.
+ * 邮箱 ysr200808@163.com
+ */
+
+class LoginInfo {
+    var username: String? = null
+    var districtcode: String? = null
+}
---
+++ app/src/main/kotlin/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.ysr.kotlinfirst
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
