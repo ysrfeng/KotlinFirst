8591e61917977cc34c2210ccef2aed521fb3220e
ysrfeng <ysr200808@163.com>
2017-05-18 16:53:55 +0800

    test

--- app/src/main/kotlin/com/ysr/kotlin/MainActivity.kt
+++ app/src/main/kotlin/com/ysr/kotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ysr.kotlinfirst;
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv_title.text = "kotin"
+        bt_post.setOnClickListener { tv_title.text = "post" }
     }
-
-
 }
